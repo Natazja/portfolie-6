16fc41d5ccbca18877dff683d97cfb06512b8b36
jlkansakar <joka0002@stud.kea.dk>
2023-12-05 14:57:17 +0100

    continued work + added ejs from npm

--- user-page/users.js
+++ user-page/users.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 // const cors = require("cors");
 const app = express();
 const port = 3002;
+// const ejs = require("ejs");
 
 const connection = mysql.createConnection( {
     host: "localhost",
@@ -20,17 +21,19 @@ connection.connect((error) => {
 });
 
 
-
+let userArray = [];
 
 
 connection.query("select * from users", function (err, result) {
     if (err) throw err;
     app.get("/users", (req, res) => {
         res.send(result)
-        return res
+        userArray = result
     })
 });
 
+
+console.log(userArray)
 //const user1 =
 // console.log(user1)
 
