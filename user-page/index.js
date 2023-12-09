5e0266239738bfa5d43578f3297c282e8d19afcd
jlkansakar <joka0002@stud.kea.dk>
2023-12-09 04:46:31 +0100

    added wrong name/email message - still needs name & email => email & password.

--- user-page/index.js
+++ user-page/index.js
@@ -1,7 +1,8 @@
+let wrongCounter = 0;
+
 fetch("http://localhost:3002/users")
 .then(res => res.json())
 .then((data)=> {
-    console.log(data)
     document.querySelector("#userCheckBTN").addEventListener("click", function userCheckFunction() {
         data.forEach(object => {
             if (object.user_name === document.querySelector("#userInput").value && object.e_mail === document.querySelector("#userInput2").value) {
@@ -10,6 +11,19 @@ fetch("http://localhost:3002/users")
                 document.querySelector("#userFavCafe").innerHTML = object.favorite_cafes;
                 document.querySelector("#userID").innerHTML = object.user_id;
             }
+
         })
+        data.forEach(object => {
+            if (object.user_name !== document.querySelector("#userInput").value || object.e_mail !== document.querySelector("#userInput2").value) {
+                wrongCounter ++;
+                if (wrongCounter === 10) { // would be good to find a way to not have it hardcoded - eg. use object.user_ID number, but i couldnt make it work :^(
+                    alert("Wrong name/email")
+                    wrongCounter -= 10;
+                }
+
+            }
+        })
+
     })
 });
+
