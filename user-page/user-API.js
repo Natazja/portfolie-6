638a3c225f4a7dfc01d5f1c963625e7729164748
jlkansakar <joka0002@stud.kea.dk>
2023-12-09 03:02:24 +0100

    fix for gitignore & .env

--- user-page/user-API.js
+++ user-page/user-API.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 const port = 3002;
 // const ejs = require("ejs");
+
+
+const result = dotenv.config();
+
+if (result.error) {
+    throw result.error
+}
+
+console.log(result.parsed);
 
 app.use(cors());
 app.use(express.json());
 
 const connection = mysql.createConnection( {
-    host: "localhost",
-    user: "root",
-    password: "Mkansakar12098",
-    database: "cafes"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 connection.connect((error) => {
