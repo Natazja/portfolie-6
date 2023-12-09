7d86166912810006d712c44ed57c166d2e44cf35
jlkansakar <joka0002@stud.kea.dk>
2023-12-09 03:54:19 +0100

    added extra to the user info page - Added another input so the search is more defined (to avoid same name issues) - Need to change from name & email => email & password.

--- user-page/index.js
+++ user-page/index.js
@@ -4,7 +4,7 @@ fetch("http://localhost:3002/users")
     console.log(data)
     document.querySelector("#userCheckBTN").addEventListener("click", function userCheckFunction() {
         data.forEach(object => {
-            if (object.user_name === document.querySelector("#userInput").value) {
+            if (object.user_name === document.querySelector("#userInput").value && object.e_mail === document.querySelector("#userInput2").value) {
                 document.querySelector("#userName").innerHTML = object.user_name;
                 document.querySelector("#userEmail").innerHTML = object.e_mail;
                 document.querySelector("#userFavCafe").innerHTML = object.favorite_cafes;
