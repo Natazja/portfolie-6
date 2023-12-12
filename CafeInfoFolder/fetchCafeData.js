6fbf0f89d359389f56fe1379cd8cf42db7eb7f44
abeldalum <abeldalum@hotmail.com>
2023-12-12 17:29:54 +0100

    asda

--- CafeInfoFolder/fetchCafeData.js
+++ CafeInfoFolder/fetchCafeData.js
@@ -1,14 +1,15 @@
-// Example GET request to /cafes endpoint
 fetch("http://localhost:4000/cafes")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
+    .then(res => res.json())
     .then(data => {
-        console.log('Data from /cafes endpoint:', data);
+        console.log("Fetched data:", data);
+        document.querySelector("#cafename").innerText = data[0].cafe_name;
+        document.querySelector("#cafename1").innerText = data[1].cafe_name;
+        document.querySelector("#cafename2").innerText = data[2].cafe_name;
+        document.querySelector("#cafename3").innerText = data[3].cafe_name;
+        document.querySelector("#cafename4").innerText = data[4].cafe_name;
+
+
     })
     .catch(error => {
-        console.error('Fetch error:', error.message);
+        console.error("Error fetching data:", error);
     });
