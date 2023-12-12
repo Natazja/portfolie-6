812c934c0761f021370d9a082c299cce886999f0
jlkansakar <joka0002@stud.kea.dk>
2023-12-12 15:18:21 +0100

    most scuff fucking cafe list but it works :^I

--- cafe-list/listIndex.js
+++ cafe-list/listIndex.js
@@ -2,21 +2,34 @@ fetch("http://localhost:3002/cafes")
     .then(res => res.json())
     .then((data)=> { // add a removal of the previously loaded cafes after the eventListener.
         document.querySelector("#dropDownSelect").addEventListener("change", function cafeList() {
-            document.querySelector("#cafe1").innerHTML = "";
-            document.querySelector("#cafe2").innerHTML = "";
-            document.querySelector("#cafe3").innerHTML = "";
-            document.querySelector("#cafe4").innerHTML = "";
-            document.querySelector("#cafe5").innerHTML = "";
-            document.querySelector("#cafe6").innerHTML = "";
-            document.querySelector("#cafe7").innerHTML = "";
-            document.querySelector("#cafe8").innerHTML = "";
-            document.querySelector("#cafe9").innerHTML = "";
-            document.querySelector("#cafe10").innerHTML = "";
-            document.querySelector("#cafe11").innerHTML = "";
-            document.querySelector("#cafe12").innerHTML = "";
-            document.querySelector("#cafe13").innerHTML = "";
+            document.querySelector("#cafe1").style.display = "none";
+            document.querySelector("#cafe2").style.display = "none";
+            document.querySelector("#cafe3").style.display = "none";
+            document.querySelector("#cafe4").style.display = "none";
+            document.querySelector("#cafe5").style.display = "none";
+            document.querySelector("#cafe6").style.display = "none";
+            document.querySelector("#cafe7").style.display = "none";
+            document.querySelector("#cafe8").style.display = "none";
+            document.querySelector("#cafe9").style.display = "none";
+            document.querySelector("#cafe10").style.display = "none";
+            document.querySelector("#cafe11").style.display = "none";
+            document.querySelector("#cafe12").style.display = "none";
+            document.querySelector("#cafe13").style.display = "none";
 
             if (document.querySelector("#dropDownSelect").value === "all") {
+                document.querySelector("#cafe1").style.display = "inline-block";
+                document.querySelector("#cafe2").style.display = "inline-block";
+                document.querySelector("#cafe3").style.display = "inline-block";
+                document.querySelector("#cafe4").style.display = "inline-block";
+                document.querySelector("#cafe5").style.display = "inline-block";
+                document.querySelector("#cafe6").style.display = "inline-block";
+                document.querySelector("#cafe7").style.display = "inline-block";
+                document.querySelector("#cafe8").style.display = "inline-block";
+                document.querySelector("#cafe9").style.display = "inline-block";
+                document.querySelector("#cafe10").style.display = "inline-block";
+                document.querySelector("#cafe11").style.display = "inline-block";
+                document.querySelector("#cafe12").style.display = "inline-block";
+                document.querySelector("#cafe13").style.display = "inline-block";
 
                 document.querySelector("#cafe1").innerHTML = data[0].cafe_name;
                 document.querySelector("#cafe2").innerHTML = data[1].cafe_name;
@@ -33,17 +46,30 @@ fetch("http://localhost:3002/cafes")
                 document.querySelector("#cafe13").innerHTML = data[12].cafe_name;
             }
             if (document.querySelector("#dropDownSelect").value === "Nørrebro") {
+                document.querySelector("#cafe1").style.display = "inline-block";
+                document.querySelector("#cafe2").style.display = "inline-block";
 
                 document.querySelector("#cafe1").innerHTML = data[3].cafe_name;
                 document.querySelector("#cafe2").innerHTML = data[5].cafe_name;
             }
             if (document.querySelector("#dropDownSelect").value === "Vesterbro") {
+                document.querySelector("#cafe1").style.display = "inline-block";
+                document.querySelector("#cafe2").style.display = "inline-block";
+                document.querySelector("#cafe3").style.display = "inline-block";
 
                 document.querySelector("#cafe1").innerHTML = data[4].cafe_name;
                 document.querySelector("#cafe2").innerHTML = data[8].cafe_name;
                 document.querySelector("#cafe3").innerHTML = data[10].cafe_name;
             }
             if (document.querySelector("#dropDownSelect").value === "København K") {
+                document.querySelector("#cafe1").style.display = "inline-block";
+                document.querySelector("#cafe2").style.display = "inline-block";
+                document.querySelector("#cafe3").style.display = "inline-block";
+                document.querySelector("#cafe4").style.display = "inline-block";
+                document.querySelector("#cafe5").style.display = "inline-block";
+                document.querySelector("#cafe6").style.display = "inline-block";
+                document.querySelector("#cafe7").style.display = "inline-block";
+                document.querySelector("#cafe8").style.display = "inline-block";
 
                 document.querySelector("#cafe1").innerHTML = data[0].cafe_name;
                 document.querySelector("#cafe2").innerHTML = data[1].cafe_name;
